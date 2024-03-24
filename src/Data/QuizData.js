d2654be874820cfcf6f4f66474dbf82f9e9f1322
unknown <kumarakshay6979@gmail.com>
2024-03-24 11:16:31 +0530

    final commit

---
+++ src/Data/QuizData.js
@@ -0,0 +1,33 @@
+export const QuizData = [
+  {
+    question:
+      "Which of the following define the basic structure of the website?",
+    options: ["HTML", "C", "Python", "JavaScript"],
+    answer: 1,
+  },
+  {
+    question: "What does CSS stand for?",
+    options: [
+      "Central Style Sheets",
+      "Cascading Style Sheets",
+      "Cascading Simple Sheets",
+      "Cars SUVs Sailboats",
+    ],
+    answer: 2,
+  },
+  {
+    question: "What does HTML stand for?",
+    options: [
+      "Hypertext Markup Language",
+      "Hypertext Markdown Language",
+      "Hyperloop Machine Language",
+      "Helicopters Terminals Motorboats Lamborginis",
+    ],
+    answer: 1,
+  },
+  {
+    question: "What year was JavaScript launched?",
+    options: ["1997", "1995", "1991", "none of the above"],
+    answer: 2,
+  },
+];
